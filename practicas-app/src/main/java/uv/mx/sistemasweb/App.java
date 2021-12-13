ee4497b38fc55e8ceba83012e99196b1cad81288
FreddySahid <freddysahid@outlook.com>
2021-12-13 15:05:08 -0600

    listo

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -4,10 +4,13 @@ package uv.mx.sistemasweb;
  * Hello world!
  *
  */
+
+import static spark.Spark.*;
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/aprobado", (req, res) -> "Espero sacar un 10");
     }
 }
